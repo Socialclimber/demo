93fde5ed84245d62cd08734264605188f975f785
socialclimber <newtnnkeng@gmail.com>
2020-05-02 20:49:33 +0200

    adding requirements doc

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def index(request):
     return render(request,'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_front_end = {
+        'search': search
+    }
+    return render(request,'demoapp/new_search.html', stuff_for_front_end)
